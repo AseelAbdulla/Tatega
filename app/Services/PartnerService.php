48524a402c0d0d60c6cda9f8b473df671a6564c7
MonaAlsharif <monaahmed32001@gmail.com>
2026-08-03 23:40:52 +0300

    Add welcome user mail with Mailtrap

--- app/Services/PartnerService.php
+++ app/Services/PartnerService.php
@@ -2,13 +2,40 @@
 
 namespace App\Services;
 
+use App\Models\Partner;
+
 class PartnerService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public function index()
     {
-        //
+        return Partner::all();
+    }
+
+
+    public function store(array $data)
+    {
+        return Partner::create($data);
+    }
+
+
+    public function show(Partner $partner)
+    {
+        return $partner;
+    }
+
+
+    public function update(Partner $partner, array $data)
+    {
+        $partner->update($data);
+
+        return $partner;
+    }
+
+
+    public function destroy(Partner $partner)
+    {
+        $partner->delete();
+
+        return true;
     }
 }
